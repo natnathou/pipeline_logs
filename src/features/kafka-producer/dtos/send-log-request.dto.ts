ea4f61a626262216b049ac102a200424ae6bd0bd
natnathou <ne.zaffran@gmail.com>
2022-08-29 20:30:25 +0300

    fix bug

--- src/features/kafka-producer/dtos/send-log-request.dto.ts
+++ src/features/kafka-producer/dtos/send-log-request.dto.ts
@@ -1,7 +1,8 @@
 import { LogDto } from '@features/kafka-producer/dtos/log.dto';
-import { IsString } from 'class-validator';
+import {IsOptional, IsString} from 'class-validator';
 
 export class SendLogRequestDto extends LogDto {
     @IsString()
+    @IsOptional()
     key: string;
 }
